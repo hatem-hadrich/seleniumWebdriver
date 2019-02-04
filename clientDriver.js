8d7aaec2466b9eab36c747fa5b8b0d90f4977e8f
hatem-hadrich <haptou28@gmail.com>
2019-02-04 11:07:10 +0100

    Headless mode in jenkins

--- clientDriver.js
+++ clientDriver.js
@@ -17,16 +17,24 @@ module.exports = {
       //setting chrome options to start the browser fully maximized
       if(global.headless === 'true') {
         var chromeOptions = {
-          'args': ['--headless', '--disable-gpu', '--no-sandbox'/*, '--disable-setuid-sandbox'*/],
+          //'browserName': 'chrome',
+          'args': ['--headless', '--disable-gpu', '--no-sandbox', '--disable-http2'/*, '--disable-setuid-sandbox'*/]
           //'binary': path
         };
         chromeCapabilities.set('chromeOptions', chromeOptions);
-      }
 
-      client = new webdriver.Builder()
-        .withCapabilities(chromeCapabilities)
-        .build();
-      return client;
+        client = new webdriver.Builder()
+          .forBrowser('chrome')
+          .usingServer("http://192.168.99.100:4444/wd/hub")
+          .withCapabilities(chromeCapabilities)
+          .build();
+        return client;
+      } else {
+        client = new webdriver.Builder()
+          .withCapabilities(chromeCapabilities)
+          .build();
+        return client;
+      }
     }
   }
 }
